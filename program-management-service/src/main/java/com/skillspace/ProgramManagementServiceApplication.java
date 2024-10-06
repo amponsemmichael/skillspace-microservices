a616d9bf54e1dd6e67fb9488e189de56c8574247
amponsemmichael <amponsem_michael@yahoo.com>
2024-10-06 12:18:28 +0000

    Feature(PMS): program management microservice setup

---
+++ program-management-service/src/main/java/com/skillspace/ProgramManagementServiceApplication.java
@@ -0,0 +1,13 @@
+package com.skillspace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProgramManagementServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProgramManagementServiceApplication.class, args);
+	}
+
+}
